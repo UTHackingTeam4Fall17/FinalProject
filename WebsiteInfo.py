daaeaebeb9c660bc159200df9687d66ccac1ba45
Michael Li <michael.eagle@utexas.edu>
2017-12-04 13:12:50 -0500

    wrote code to read in website attack info

--- WebsiteInfo.py
+++ WebsiteInfo.py
@@ -8,8 +8,32 @@ class WebsiteInfo:
         self.port = port
 
     def get_attack_info(self):
-        return self.ip_address, self.service_mode, self.address_and_details, self.port
+        return self.ip_address, self.port, self.service_mode, self.address_and_details
 
+    def __str__(self):
+        return '{} -- {}:{} {}, {}'.format(self.website_name, self.ip_address, self.port, self.address_and_details, self.service_mode)
 
+    def __repr__(self):
+        return str(self)
+
+from os import listdir
+websites_dir = 'website_info/'
 def get_all_websites():
-    
+    websites = {}
+
+    files = listdir(websites_dir)
+    for website_name in files:
+        with open(websites_dir + website_name) as f:
+            ip_addr = f.readline().rstrip()
+            port = f.readline().rstrip()
+            service_mode = f.readline().rstrip()
+            address_and_details = f.readline().strip()
+            
+            websites[website_name] = WebsiteInfo(website_name, ip_addr, service_mode, address_and_details, port)
+
+    return websites
+
+if __name__ == '__main__':
+    websites = get_all_websites()
+    for k, v in websites.items():
+        print(v)
